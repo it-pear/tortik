d3bdf85422d7253be6a8e52a2ae8d5cfbc0013ff
itpear <yurecbilnovgelarm@gmail.com>
2021-12-26 16:24:51 +0300

    sdf

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
   const nuxt = new Nuxt(config)
-  
+
   const {
-    host = process.env.HOST || '62.113.98.94',
+    host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000
   } = nuxt.options.server
 
