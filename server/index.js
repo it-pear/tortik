4acdc6776abe005dc9191aad740c456f56a5502b
itpear <yurecbilnovgelarm@gmail.com>
2021-12-26 19:53:29 +0300

    mongoose

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ async function start() {
   const nuxt = new Nuxt(config)
   
   const {
-    host = process.env.HOST || 'localhost',
+    host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000
   } = nuxt.options.server
 
